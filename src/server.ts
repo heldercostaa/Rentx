e7b9c921647ea81c9b8852ab132990ab9c7e5e19
Helder Costa <heldercostaaa@gmail.com>
2022-07-27 23:15:01 -0300

    :sparkles: Add route to create categories

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World!" });
-});
+app.use(categoriesRoutes);
 
 const port = 3333;
 app.listen(port, () => console.log(`🚀 Server started on port: ${port}`));
