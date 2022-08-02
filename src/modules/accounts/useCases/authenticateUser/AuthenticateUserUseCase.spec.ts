6065cb56c86a6c47b205cc90a87f95700e175c47
Helder Costa <heldercostaaa@gmail.com>
2022-08-02 18:55:53 -0300

    :wrench: Add paths to tsconfig.json :wrench: Adjust imports on .eslintrc.json :heavy_plus_sign: Add tsconfig-paths lib :package: Call tsconfig-paths on scripts :art: Refactor code to use ts paths

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -1,5 +1,6 @@
-import { AppError } from "../../../../errors/AppError";
-import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { AppError } from "@errors/AppError";
+import { InMemoryUsersRepository } from "@modules/accounts/repositories/in-memory/InMemoryUsersRepository";
+
 import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
 import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
 
--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,13 @@ import "express-async-errors";
 import "reflect-metadata";
 import swaggerUi from "swagger-ui-express";
 
-import { AppError } from "./errors/AppError";
+import { AppError } from "@errors/AppError";
+import "@shared/container";
+
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
 import "./database";
-import "./shared/container";
 
 const app = express();
 app.use(express.json());
