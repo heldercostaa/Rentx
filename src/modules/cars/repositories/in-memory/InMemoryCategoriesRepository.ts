b9f78ebd85d9030f95e14971853e2a63a3d50db6
Helder Costa <heldercostaaa@gmail.com>
2022-08-02 18:23:50 -0300

    :white_check_mark: Add 'should be able to create a new category' :white_check_mark: Add 'should not be able to create a new category with existing name'

---
+++ src/modules/cars/repositories/in-memory/InMemoryCategoriesRepository.ts
@@ -0,0 +1,32 @@
+import { Category } from "../../entities/Category";
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from "../ICategoriesRepository";
+
+class InMemoryCategoriesRepository implements ICategoriesRepository {
+  categories: Category[] = [];
+
+  async create({ name, description }: ICreateCategoryDTO) {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+    });
+
+    this.categories.push(category);
+  }
+
+  async list() {
+    return this.categories;
+  }
+
+  async findByName(name: string) {
+    const category = this.categories.find((category) => category.name === name);
+
+    return category;
+  }
+}
+
+export { InMemoryCategoriesRepository };
---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,54 @@
+import { AppError } from "../../../../errors/AppError";
+import { InMemoryCategoriesRepository } from "../../repositories/in-memory/InMemoryCategoriesRepository";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+
+let createCategoryUseCase: CreateCategoryUseCase;
+let inMemoryCategoriesRepository: InMemoryCategoriesRepository;
+
+describe("Create Category", () => {
+  beforeEach(() => {
+    inMemoryCategoriesRepository = new InMemoryCategoriesRepository();
+    createCategoryUseCase = new CreateCategoryUseCase(
+      inMemoryCategoriesRepository
+    );
+  });
+
+  it("should be able to create a new category", async () => {
+    const category = {
+      name: "SUV",
+      description:
+        "Cars that combine elements of road-going passenger cars with features from off-road vehicles.",
+    };
+
+    await createCategoryUseCase.execute({
+      name: category.name,
+      description: category.description,
+    });
+
+    const createdCategory = await inMemoryCategoriesRepository.findByName(
+      category.name
+    );
+
+    expect(createdCategory).toHaveProperty("id");
+  });
+
+  it("should not be able to create a new category with existing name", async () => {
+    expect(async () => {
+      const category = {
+        name: "SUV",
+        description:
+          "Cars that combine elements of road-going passenger cars with features from off-road vehicles.",
+      };
+
+      await createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description,
+      });
+
+      await createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description,
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
