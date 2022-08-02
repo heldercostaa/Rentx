0377f05a9473b31e51d21d1876c1227f636fa85f
Helder Costa <heldercostaaa@gmail.com>
2022-08-02 00:54:11 -0300

    :sparkles: Change application errors to AppError :sparkles: Add custom AppError for responses :heavy_plus_sign: Add express-async-errors

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,6 +1,7 @@
 import { hash } from "bcrypt";
 import { inject, injectable } from "tsyringe";
 
+import { AppError } from "../../../../errors/AppError";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -15,7 +16,7 @@ class CreateUserUseCase {
     const userExists = await this.userRepository.findByEmail(email);
 
     if (userExists) {
-      throw new Error("User already exists");
+      throw new AppError("User already exists");
     }
 
     const passwordHash = await hash(password, 8);
