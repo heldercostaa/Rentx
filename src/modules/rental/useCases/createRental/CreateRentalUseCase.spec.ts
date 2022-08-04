dc5626bb6ee7037e560649b04e52f2d06fbb3e24
Helder Costa <heldercostaaa@gmail.com>
2022-08-04 12:53:34 -0300

    :heavy_plus_sign: Add dayjs :sparkles: Do not allow to rent if return date is less than 24 hour

--- src/modules/rental/useCases/createRental/CreateRentalUseCase.spec.ts
+++ src/modules/rental/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -1,3 +1,5 @@
+import dayjs from "dayjs";
+
 import { InMemoryRentalRepository } from "@modules/rental/infra/typeorm/repositories/in-memory/InMemoryRentalRepository";
 import { AppError } from "@shared/errors/AppError";
 
@@ -7,6 +9,8 @@ let createRentalUseCase: CreateRentalUseCase;
 let inMemoryRentalRepository: InMemoryRentalRepository;
 
 describe("Create Rental", () => {
+  const nowPlus1Day = dayjs().add(1, "day").toDate();
+
   beforeEach(() => {
     inMemoryRentalRepository = new InMemoryRentalRepository();
     createRentalUseCase = new CreateRentalUseCase(inMemoryRentalRepository);
@@ -16,7 +20,7 @@ describe("Create Rental", () => {
     const rental = await createRentalUseCase.execute({
       carId: "d961210f-ff68-4edc-adfe-22b48c1a065a",
       userId: "afbc032b-3796-46b4-baaf-115e208f398e",
-      expectedReturnDate: new Date(),
+      expectedReturnDate: nowPlus1Day,
     });
 
     expect(rental).toHaveProperty("id");
@@ -28,13 +32,13 @@ describe("Create Rental", () => {
       await createRentalUseCase.execute({
         carId: "d961210f-ff68-4edc-adfe-22b48c1a065a",
         userId: "64fc94f4-359f-4664-8ddd-1d3e4a326b0b",
-        expectedReturnDate: new Date(),
+        expectedReturnDate: nowPlus1Day,
       });
 
       await createRentalUseCase.execute({
         carId: "d961210f-ff68-4edc-adfe-22b48c1a065a",
         userId: "afbc032b-3796-46b4-baaf-115e208f398e",
-        expectedReturnDate: new Date(),
+        expectedReturnDate: nowPlus1Day,
       });
     }).rejects.toBeInstanceOf(AppError);
   });
@@ -44,13 +48,23 @@ describe("Create Rental", () => {
       await createRentalUseCase.execute({
         carId: "18b300b8-b2a2-4167-8651-4858f0a92377",
         userId: "afbc032b-3796-46b4-baaf-115e208f398e",
-        expectedReturnDate: new Date(),
+        expectedReturnDate: nowPlus1Day,
       });
 
       await createRentalUseCase.execute({
         carId: "d961210f-ff68-4edc-adfe-22b48c1a065a",
         userId: "afbc032b-3796-46b4-baaf-115e208f398e",
-        expectedReturnDate: new Date(),
+        expectedReturnDate: nowPlus1Day,
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should not be able to create a rental with rental time less than 24 hour", async () => {
+    expect(async () => {
+      await createRentalUseCase.execute({
+        carId: "18b300b8-b2a2-4167-8651-4858f0a92377",
+        userId: "afbc032b-3796-46b4-baaf-115e208f398e",
+        expectedReturnDate: dayjs().toDate(),
       });
     }).rejects.toBeInstanceOf(AppError);
   });
--- src/modules/rental/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rental/useCases/createRental/CreateRentalUseCase.ts
@@ -1,11 +1,18 @@
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+
 import { IRentalRepository } from "@modules/rental/repositories/IRentalRepository";
 import { AppError } from "@shared/errors/AppError";
+
+dayjs.extend(utc);
 
 interface IRequest {
   userId: string;
   carId: string;
   expectedReturnDate: Date;
 }
+
+const MINIMUM_RENT_HOURS = 24;
 
 class CreateRentalUseCase {
   constructor(private rentalRepository: IRentalRepository) {}
@@ -21,6 +28,22 @@ class CreateRentalUseCase {
 
     if (isUserRenting) {
       throw new AppError("There is already a rental in progress for this user");
+    }
+
+    const expectedReturnDateFormatted = dayjs(expectedReturnDate)
+      .utc()
+      .local()
+      .format();
+
+    const nowFormatted = dayjs().utc().local().format();
+
+    const compare = dayjs(expectedReturnDateFormatted).diff(
+      nowFormatted,
+      "hours"
+    );
+
+    if (compare < MINIMUM_RENT_HOURS) {
+      throw new AppError("Rental time should be at least 24 hours");
     }
 
     const rental = await this.rentalRepository.create({
