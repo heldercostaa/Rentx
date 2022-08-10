15ba00195d8eb55d28567d8dbcd785873045e065
Helder Costa <heldercostaaa@gmail.com>
2022-08-09 21:32:59 -0300

    :bug: Fix UploadCarImage use case to use S3

--- src/modules/car/useCases/uploadCarImages/UploadCarImagesUseCase.ts
+++ src/modules/car/useCases/uploadCarImages/UploadCarImagesUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from "tsyringe";
 
 import { ICarImageRepository } from "@modules/car/repositories/ICarImageRepository";
+import { IStorageProvider } from "@shared/container/providers/StorageProvider/IStorageProvider";
 
 interface IRequest {
   carId: string;
@@ -11,12 +12,16 @@ interface IRequest {
 class UploadCarImagesUseCase {
   constructor(
     @inject("CarImageRepository")
-    private carImageRepository: ICarImageRepository
+    private carImageRepository: ICarImageRepository,
+
+    @inject("StorageProvider")
+    private storageProvider: IStorageProvider
   ) {}
 
   async execute({ carId, imageNames }: IRequest) {
     imageNames.map(async (imageName) => {
       await this.carImageRepository.create({ carId, imageName });
+      await this.storageProvider.save(imageName, "cars");
     });
   }
 }
