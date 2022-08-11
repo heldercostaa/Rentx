9d01d1bfef1ac68797da8a7ec3a377cdd37f60ec
Helder Costa <heldercostaaa@gmail.com>
2022-08-10 21:23:38 -0300

    :wrench: Comment app from docker

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,11 +1,12 @@
 import { createConnection, getConnectionOptions } from "typeorm";
 
-export default async (host = "database") => {
+// export default async (host = "database") => {
+export default async () => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === "test" ? "localhost" : host,
+      // host: process.env.NODE_ENV === "test" ? "localhost" : host,
       database:
         process.env.NODE_ENV === "test"
           ? "rentxdb_tests"
